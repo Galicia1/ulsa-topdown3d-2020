adfea2b141cbde007d35d458a69bdd7e82b46a8f
Galicia <galiciavzla@gmail.com>
2020-03-30 11:55:39 -0700

    movimiento 3D

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Axis*moveSpeed*Time.deltaTime);
+    }
+
+    Vector3 Axis
+    {
+        get => new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
+    }
+}
